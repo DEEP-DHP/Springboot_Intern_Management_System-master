7fd393839238fb1f5cf26fa796469a1fde8faa56
Deep Patel <96335046+DEEP-DHP@users.noreply.github.com>
2025-01-23 14:10:23 +0530

    Add files via upload

--- src/main/java/com/rh4/models/ReportFilter.java
+++ src/main/java/com/rh4/models/ReportFilter.java
@@ -4,25 +4,27 @@ import java.sql.Date;
 
 
 public class ReportFilter {
-    private String college;
-    private String branch;
-    private String guide;
-    private String domain;
-    private Date startDate;
-    private Date endDate;
-    private String cancelled;
-    private String format;
+	private String college;
+	private String branch;
+	private String guide;
+	private String domain;
+	private String degree;
+	private Date startDate;
+	private Date endDate;
+	private String cancelled;
+	private String format;
 	public ReportFilter() {
 		super();
 	}
-	public ReportFilter(String college, String branch, String guide, String domain, Date startDate, Date endDate,
-			String cancelled, String format) {
+	public ReportFilter(String college, String branch, String guide, String domain, String degree, Date startDate, Date endDate,
+						String cancelled, String format) {
 		super();
 		this.college = college;
 		this.branch = branch;
 		this.guide = guide;
 		this.format = format;
 		this.domain = domain;
+		this.degree = degree;
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.cancelled = cancelled;
@@ -54,9 +56,7 @@ public class ReportFilter {
 	public Date getStartDate() {
 		return startDate;
 	}
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
+	public void setStartDate(Date startDate) { this.startDate = startDate; }
 	public Date getEndDate() {
 		return endDate;
 	}
@@ -75,6 +75,7 @@ public class ReportFilter {
 	public void setFormat(String format) {
 		this.format = format;
 	}
-	
-	   
+	public String getDegree() {return degree; }
+	public void setDegree(String degree) {this.degree = degree; }
+
 }
