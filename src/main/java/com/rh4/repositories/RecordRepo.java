23efa7be9ecbcd79c5b0ef0aa01ca07235d827d6
Deep Patel <96335046+DEEP-DHP@users.noreply.github.com>
2025-02-04 14:10:37 +0530

    Add files via upload

--- src/main/java/com/rh4/repositories/RecordRepo.java
+++ src/main/java/com/rh4/repositories/RecordRepo.java
@@ -2,6 +2,8 @@ package com.rh4.repositories;
 
 import com.rh4.entities.RRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,9 @@ import java.util.List;
 @Repository
 public interface RecordRepo extends JpaRepository<RRecord, Long> {
     List<RRecord> findByInternId(String internId);
+
+    @Query("SELECT r FROM RRecord r WHERE LOWER(r.internId) LIKE LOWER(CONCAT('%', :search, '%')) OR LOWER(r.collegeName) LIKE LOWER(CONCAT('%', :search, '%'))")
+    List<RRecord> findByInternIdContainingIgnoreCaseOrCollegeNameContainingIgnoreCase(@Param("search") String search);
+
+    List<RRecord> findByStatus(String status);
 }
