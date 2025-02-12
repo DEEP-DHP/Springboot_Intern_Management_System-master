80a3d89b343f502df019b4b8bf140900340496bf
Deep Patel <96335046+DEEP-DHP@users.noreply.github.com>
2025-02-12 13:10:26 +0530

    Add files via upload

---
+++ src/main/java/com/rh4/repositories/ThesisStorageRepo.java
@@ -0,0 +1,12 @@
+package com.rh4.repositories;
+
+import com.rh4.entities.ThesisStorage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ThesisStorageRepo extends JpaRepository<ThesisStorage, Long> {
+    Optional<ThesisStorage> findById(Long id);
+}
